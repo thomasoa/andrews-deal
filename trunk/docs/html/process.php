08578cf5c14e9150e0d9e1e7e402cead1aad5a38
thomaso <thomaso@6e66fcdd-4656-0410-bfdc-eb0846995c51>
2008-06-05 19:32:03 +0000

    Updating version information for next release

--- trunk/docs/html/process.php
+++ trunk/docs/html/process.php
@@ -2,12 +2,13 @@
    $command = array_shift($argv);
    $output = array_shift($argv);
    $file = array_shift($argv);
-   $version = "3.1.3";
+   $version = "3.1.4";
    $shorterVersion = "3.1";
-   $package = "deal313";
+   $package = "deal314";
    if ($output == 'site') {
      $bridgeFantasia = "../";
    } else {
      $bridgeFantasia = "http://bridge.thomasoandrews.com/";
    }
+   $copyright = "1996-2008";
    include($file);
