9b8a367c1adc2f965960720e318cfe84abc42cfa
thomaso <thomaso@6e66fcdd-4656-0410-bfdc-eb0846995c51>
2008-06-10 21:27:31 +0000

    Updated for next release - 3.1.5

--- docs/html/process.php
+++ docs/html/process.php
@@ -2,9 +2,9 @@
    $command = array_shift($argv);
    $output = array_shift($argv);
    $file = array_shift($argv);
-   $version = "3.1.4";
+   $version = "3.1.5";
    $shorterVersion = "3.1";
-   $package = "deal314";
+   $package = "deal315";
    if ($output == 'site') {
      $bridgeFantasia = "../";
    } else {
