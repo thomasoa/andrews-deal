3268da48b31b31ceb353c2aade8bbca416eedc79
thomaso <thomaso@6e66fcdd-4656-0410-bfdc-eb0846995c51>
2008-06-10 20:12:49 +0000

    Simplified deal.tcl by putting most code into features

--- trunk/deal.tcl
+++ trunk/deal.tcl
@@ -3,7 +3,7 @@
 #
 # Copyright (C) 1996-2001, Thomas Andrews
 #
-# $Id: deal.tcl,v 1.12 2008-06-10 19:58:50 thomaso Exp $
+# $Id: deal.tcl,v 1.13 2008-06-10 20:12:49 thomaso Exp $
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,139 +20,14 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-proc dds_reset_command {} {
-  dds_reset
-  deal_reset_cmds [list dds_reset_command]
-}
+# Mac default value for tcl_library
+#set tcl_library /System/Library/Frameworks/Tcl.framework/Versions/8.4/Resources/Scripts
 
+# Windows default value for tcl_library
+#set tcl_library C:/tcl/lib/tcl8.5
 
-namespace eval deal {
+catch { deal_init_tcl }
+#puts $tcl_library
+#puts $auto_path
 
-    variable metadata
-    variable unicode 1
-
-    #
-    # Put data in the cache, to be unset at next call
-    # to deal_deck
-    #
-    proc metadata {name code} {
-	variable metadata
-	if {![info exists metadata($name)]} {
-	    if {[catch {set metadata($name) [uplevel $code]}]} {
-		global errorInfo
-		puts stderr "Error: $errorInfo"
-	    } else {
-		deal_reset_cmds [list unset ::deal::metadata($name)]
-	    }
-	}
-	return $metadata($name)
-    }
-
-    proc loop {} {
-	next
-	write
-    }
-
-
-    proc input {format args} {
-	uplevel #0 [list source "input/$format.tcl" ]
-	set command [list "${format}::set_input"]
-	foreach arg $args {
-	    lappend command $arg
-	}
-	uplevel #0 $command
-    }
-
-    proc debug {args} {
-       puts stderr $args
-    }
-
-    # Cause an error if any hand stacking has occured
-    proc nostacking {} {
-        set format [uplevel {namespace current}]
-        proc ::stack_hand {args} \
-		"error \"No hand stacking with input format $format\""
-        proc ::stack_cards {args} \
-		"error \"No card stacking with input format $format\""
-        foreach hand {south north east west} {
-          foreach holding [stacked $hand] {
-             if {[holding length $holding]!=0} {
-                error "Stacking cards is not consistent with input format $format"
-             }
-          }
-        }
-    }
-}
-
-# These two routines used to be defined in C, but it's better for them
-# to fit the pattern of shape functions.
-
-deal_init_tcl
-if {[string equal [info commands dds_reset] "dds_reset"]} {
-  dds_reset_command
-}
-shapecond balanced {($h<5)&&($s<5)&&($s*$s+$h*$h+$d*$d+$c*$c)<=47}
-shapecond semibalanced {$h<=5&&$s<=5&&$d<=6&&$c<=6&&$c>=2&&$d>=2&&$h>=2&&$s>=2}
-shapecond AnyShape {1}
-source format/default
-
-#
-#  The three routines, joinclass, negateclass, intersectclass, used to be
-#  implemented in C, but were never documented and recently crashed Deal 3.0.x
-#  when called.  I've reimplemented them here in pure Tcl.
-#
-proc joinclass {newclass args} {
-    set values [list 0]
-    foreach class $args {
-	lappend values "\[$class eval \$s \$h \$d \$c\]"
-    }
-
-    shapecond ___tempclass [join $values "||"]
-
-    # make sure it is compiled first - use temporary name
-    # in case we are re-using an old name for a class
-    ___tempclass eval 13 0 0 0
-    rename ___tempclass $newclass
-}
-
-proc negateclass {newclass class} {
-    shapecond ___tempclass "!\[$class eval \$s \$h \$d \$c\]"
-    ___tempclass eval 13 0 0 0
-    rename ___tempclass $newclass
-}
-
-proc intersectclass {newclass args} {
-
-    set values [list 1]
-    foreach class $args {
-	lappend values "\[$class eval \$s \$h \$d \$c\]"
-    }
-    shapecond ___tempclass [join $values "&&"]
-    ___tempclass eval 13 0 0 0
-    rename ___tempclass $newclass
-}
-
-namespace eval deal {
-
-    variable tricksCmd ::tricks
-    variable tricksCache "tricks"
-
-    #
-    # "tricks" - Determine the number of tricks declarer can
-    # make in the denomination given.
-    #
-    proc tricks {declarer denom} {
-	variable tricksCmd
-	variable tricksCache
-	::deal::metadata "$tricksCache.$declarer.$denom" [list $tricksCmd $declarer $denom]
-    }
-
-}
-
-#
-# Returns all of the hands in a list
-#
-proc full_deal {} {
-  return [list [north] [east] [south] [west]]
-}
-
+source lib/features.tcl
