8cc11b53bf73a27543aeb1077d6bc1d28f99c2c9
thomasoa <thomasoa@6e66fcdd-4656-0410-bfdc-eb0846995c51>
2008-06-19 19:07:31 +0000

    Added seperate inline functions file for dds

---
+++ trunk/ddsInline.h
@@ -0,0 +1,31 @@
+#ifndef __DDS_INLINE_H__
+#define __DDS_INLINE_H__
+
+#include "ddsInterface.h"
+
+inline int RelativeHand(int hand, int relative) {
+  return (hand + relative)&3;
+}
+
+inline int partner(int hand) {
+  return (hand^2); /* slightly faster */
+}
+
+inline int lho(int hand) {
+  return RelativeHand(hand,1);
+}
+
+inline int rho(int hand) {
+  return RelativeHand(hand,3);
+}
+
+inline holding_t BitRank(int rank) {
+  /*
+   * Trick calculation
+   * Equivalent to 1<<(rank-2) for rank>=2, and 0 for rank<2.
+   */
+  return (1<<rank)>>2;
+}
+
+
+#endif
