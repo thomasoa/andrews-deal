007705527c7f87201568ecfacc4f44df8f0d8815
thomasoa <thomasoa@6e66fcdd-4656-0410-bfdc-eb0846995c51>
2009-06-11 05:39:42 +0000

    Renamed analytics file; added function to include them

--- trunk/docs/html/process.php
+++ trunk/docs/html/process.php
@@ -11,4 +11,11 @@
      $bridgeFantasia = "http://bridge.thomasoandrews.com/";
    }
    $copyright = "1996-2008";
+   function analytics() {
+     global $output;
+     if ($output == 'site') {
+       include("analytics.htmlf");
+     }
+   }
+
    include($file);
