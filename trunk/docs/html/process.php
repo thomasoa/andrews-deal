34e2d8ed92bd3986a7a3111bf68cf5a1344350a5
thomasoa <thomasoa@6e66fcdd-4656-0410-bfdc-eb0846995c51>
2010-02-16 21:20:48 +0000

    Change files for next release (3.1.9)

--- trunk/docs/html/process.php
+++ trunk/docs/html/process.php
@@ -2,15 +2,15 @@
    $command = array_shift($argv);
    $output = array_shift($argv);
    $file = array_shift($argv);
-   $version = "3.1.8";
+   $version = "3.1.9";
    $shorterVersion = "3.1";
-   $package = "deal318";
+   $package = "deal319";
    if ($output == 'site') {
      $bridgeFantasia = "../";
    } else {
      $bridgeFantasia = "http://bridge.thomasoandrews.com/";
    }
-   $copyright = "1996-2008";
+   $copyright = "1996-2010";
    function analytics() {
      global $output;
      if ($output == 'site') {
