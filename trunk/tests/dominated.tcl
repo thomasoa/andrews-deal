1acb711b51c19cd66be3f2d96a2bc505eb9d64bf
thomasoa <thomasoa@6e66fcdd-4656-0410-bfdc-eb0846995c51>
2010-02-28 21:52:22 +0000

    Added dominated function and tests

---
+++ trunk/tests/dominated.tcl
@@ -0,0 +1,28 @@
+source lib/dominated.tcl
+
+#
+# The "dominated" routine returns the next smaller holding values
+#
+proc testDominated {holding expected} {
+    if {$holding == ""} {
+        set id "void"
+    } else {
+        set id $holding
+    }
+    test $id "dominated holding {$holding}" $expected
+}
+
+testDominated AKJx {AQJx AKTx}
+testDominated AKJx {AQJx AKTx}
+testDominated AQxxxx {KQxxxx AJxxxx}
+testDominated AQJ {KQJ AQT}
+testDominated AQTxx {KQTxx AJTxx AQxxx}
+testDominated xxxxx {}
+testDominated Txxxxxxxx {}
+testDominated AKQJT {AKQJx}
+testDominated KQJT {KQJx}
+testDominated QJT {QJx}
+testDominated JT {Jx}
+testDominated AKQ {AKJ}
+testDominated KQ {KJ}
+testDominated {} {}
