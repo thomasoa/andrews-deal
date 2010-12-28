7e84f62bb7a0445e17d0da1682a7c405c1b40341
thomasoa <thomasoa@6e66fcdd-4656-0410-bfdc-eb0846995c51>
2010-12-28 16:01:42 +0000

    Added test for issue #24

---
+++ tests/dds-bug.tcl
@@ -0,0 +1,12 @@
+set diagram {
+        {432 QJT9 32 AQJT}
+        {A765 K32 87654 5}
+        {KQ A8765 AQ K432}
+        {JT98 4 KJT9 9876}
+}
+
+#
+# First time I get the result of zero
+#
+test equal {dds -leader west -diagram $diagram -trick JS south hearts} 11
+
